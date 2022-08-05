d6bc98d89a94854a070932891e07f6c6eecaa2bd
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-05 13:50:07 -0600

    Creamos el archivo de javascript del menú

---
+++ vistas/js/header.js
@@ -0,0 +1,11 @@
+function toggleMenu () {  
+
+  const navbar = document.querySelector('.navbar');
+  const burger = document.querySelector('.burger');
+  burger.addEventListener('click', (e) => {    
+    navbar.classList.toggle('show-nav');
+  })
+
+}
+
+toggleMenu()
