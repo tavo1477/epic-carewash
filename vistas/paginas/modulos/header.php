4f15f9470552ce51232f5bcfd81ab5d9c71118be
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-05 13:52:40 -0600

    Creamos el menú en el header

--- vistas/paginas/modulos/header.php
+++ vistas/paginas/modulos/header.php
@@ -1,1 +1,35 @@
-<h1>MENÚ</h1>
+<!--=============================================
+MENÚ
+=============================================-->
+
+<header class="main-header">
+ 
+	<nav class="navbar l-container" role="navigation">
+		
+		<a href="#">
+			
+			<div class="navbar__logo">Logo</div>
+
+		</a>
+
+		 <ul class="navbar__links">
+		 	
+		 	<li class="navbar__link first"><a href="#">Inicio</a></li>
+	      	<li class="navbar__link second"><a href="#">¿Quiénes somos?</a></li>
+	      	<li class="navbar__link third"><a href="#">Servicios</a></li>
+	      	<li class="navbar__link four"><a href="#">Portafolio</a></li>
+	      	<li class="navbar__link fifth"><a href="#">Contacto</a></li>
+
+	    </ul>  
+
+	    <button class="burger">
+
+	      <span class="bar"></span>  
+
+	    </button>   
+
+		 </ul>
+
+	</nav>
+
+</header>
