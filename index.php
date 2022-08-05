9f850561d87684457dbdca710c02e6a851a37e15
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-05 12:51:53 -0600

    Agregando el controlador de la ruta del sitio web

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
 require_once "controladores/plantillaControlador.php";
+require_once "controladores/rutaControlador.php";
 
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
