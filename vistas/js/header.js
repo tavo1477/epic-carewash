3c758b2a323263025ba0529be18c551d58669eb8
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-07 15:36:10 -0600

    Creando archivo js del menú

--- vistas/js/header.js
+++ vistas/js/header.js
@@ -1,11 +1,15 @@
-function toggleMenu () {  
+/*=============================================
+MENÚ
+=============================================*/
 
-  const navbar = document.querySelector('.navbar');
-  const burger = document.querySelector('.burger');
-  burger.addEventListener('click', (e) => {    
-    navbar.classList.toggle('show-nav');
-  })
-
+const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId),
+          nav = document.getElementById(navId)
+    if(toggle && nav) {
+        toggle.addEventListener('click', () => {
+            nav.classList.toggle('show')            
+        })
+    }    
 }
 
-toggleMenu()
+showMenu('main-menu-toggle','main-nav');
