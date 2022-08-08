559ec3a0bd3ae5a13a77125428040c21b99c6261
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-08 17:36:15 -0600

    Creando el archivo de servicios

---
+++ vistas/paginas/modulos/servicios.php
@@ -0,0 +1,76 @@
+<!--=====================================
+SERVICIOS
+======================================-->
+
+<section class="servicios l-section">
+	
+	<div class="servicios-container l-container">
+		
+		<article class="servicios-item">
+			
+			<div class="servicios-img">
+				
+				<img src="<?php echo $ruta?>vistas/images/servicios/lavado.png" alt="">				
+
+			</div>
+
+			<h2 class="servicios-title">Lavado de autos</h2>
+
+		</article>
+
+		<article class="servicios-item">
+			
+			<div class="servicios-img">
+				
+				<img src="<?php echo $ruta?>vistas/images/servicios/aspirado.png" alt="">				
+
+			</div>
+
+			<h2 class="servicios-title">Aspirado</h2>
+
+		</article>
+
+		<article class="servicios-item">
+			
+			<div class="servicios-img">
+				
+				<img src="<?php echo $ruta?>vistas/images/servicios/pulido-vidrios.png" alt="">				
+
+			</div>
+
+			<h2 class="servicios-title">Pulido de vidrios</h2>
+
+		</article>	
+
+		<article class="servicios-item">
+			
+			<div class="servicios-img">
+				
+				<img src="<?php echo $ruta?>vistas/images/servicios/booking.png" alt="">				
+
+			</div>
+
+			<h2 class="servicios-title">Sistema de reserva de citas en línea</h2>
+
+		</article>
+
+		<div class="service-calling l-container">
+
+			<div class="service-calling__text">
+
+				<p><strong>Ofreciendo un servicio rápido y eficiente para mantener su auto limpio</strong></p>				
+
+			</div>	
+
+			<div class="service-calling__text">
+				
+				<p class="movil-call">Llamar para reservar <span>+506 8540-0987</span></p>
+
+			</div>
+
+		</div>		
+
+	</div>
+
+</section>
+
