e9608931d3cdfba35288dc0101e0c02d97504c34
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-09 16:01:23 -0600

    Creamos la vista de la sección de paquetes

---
+++ vistas/paginas/modulos/paquetes.php
@@ -0,0 +1,225 @@
+<!--=====================================
+PAQUETES
+======================================-->
+
+<section class="paquetes l-section">
+
+	<h2 class="paquetes-smallTitle center-content">Precio de lavado</h2>
+
+	<h1 class="paquetes-title center-content">Escoge tu paquete</h1>
+	
+	<div class="autos-container l-container">		
+					
+		<div class="autos-img">
+
+			<img src="<?php echo $ruta ?>vistas/images/autos/sedan_01.png" alt="">	
+
+			<h4 class="auto-img__title">Sedán</h4>
+
+		</div>
+
+		<div class="autos-img">
+
+			<img src="<?php echo $ruta ?>vistas/images/autos/pickup_03.png" alt="">	
+
+			<h4 class="auto-img__title">Pickup</h4>			
+
+		</div>
+
+		<div class="autos-img">
+
+			<img src="<?php echo $ruta ?>vistas/images/autos/suv_02.png" alt="">
+
+			<h4 class="auto-img__title">SUV</h4>				
+
+		</div>
+
+		<div class="autos-img">
+
+			<img src="<?php echo $ruta ?>vistas/images/autos/minibus_04.png" alt="">	
+
+			<h4 class="auto-img__title">Minibus</h4>			
+
+		</div>		
+
+	</div>
+
+	<div class="paquetes-container l-container">
+		
+		<div class="paquetes-item">
+			
+			<h2 class="paquetes-item__title">Lavado Básico</h2>
+
+			<h3 class="paquetes-item__price"><span class="price-moneda"></span> <span class="price">6 000</span></h3>
+
+			<h4 class="paquetes-item__duracion">15 minutos</h4>
+
+			<hr>
+
+			<div class="paquetes-list">
+				
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Pre lavado</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Lavado</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Aspirado</h3>
+
+				</div>
+
+				<a href="#" class="btn-flip" data-back="Escoga el Plan" data-front="Escoga el Paquete" style="margin-top: 9.5rem;"></a>
+
+			</div>
+
+		</div>
+
+		<div class="paquetes-item">
+			
+			<h2 class="paquetes-item__title">Lavado Plus</h2>
+
+			<h3 class="paquetes-item__price"><span class="price-moneda"></span> <span class="price">10 000</span></h3>
+
+			<h4 class="paquetes-item__duracion">30 minutos</h4>
+
+			<hr>
+
+			<div class="paquetes-list">
+				
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Pre lavado</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Lavado</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Aspirado</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Detallado de partes negras</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Aromatizante</h3>
+
+				</div>
+
+				<a href="#" class="btn-flip" data-back="Escoga el Paquete" data-front="Escoga el Paquete" style="margin-top: 5rem;"></a>
+
+			</div>
+
+		</div>
+
+		<div class="paquetes-item">
+			
+			<h2 class="paquetes-item__title">Lavado Premium</h2>
+
+			<h3 class="paquetes-item__price"><span class="price-moneda"></span> <span class="price">20 000</span></h3>
+
+			<h4 class="paquetes-item__duracion">45 minutos</h4>
+
+			<hr>
+
+			<div class="paquetes-list">
+				
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Pre lavado</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Lavado</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Aspirado</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Detallado de partes negras</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Aromatizante</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Descontaminación</h3>
+
+				</div>
+
+				<div class="paquetes-list-item l-container">
+					
+					<img src="<?php echo $ruta ?>vistas/images/check.png" alt="">
+
+					<h3 class="paquetes-list-title">Pulido de Focos</h3>
+
+				</div>
+
+				<a href="#" class="btn-flip" data-back="Escoga el Paquete" data-front="Escoga el Paquete" style="margin-top: .5rem;" ></a>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</section>
