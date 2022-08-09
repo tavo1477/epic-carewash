960012260510a55371dd292708a8149b318b1245
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-09 12:48:25 -0600

    Creamos la vista de lasección de nosotros

---
+++ vistas/paginas/modulos/nosotros.php
@@ -0,0 +1,37 @@
+<!--=====================================
+NOAOTROS
+======================================-->
+
+<article class="nosotros-app l-section">
+	
+	<main class="nosotros-main l-container">
+		
+		<aside class="main-aside">
+			
+			<h2 class="aside-smallTitle"><strong>Nosotros</strong></h2>
+
+			<h2 class="aside-title">Servicios profesionales de lavado y detallado de autos</h2>
+
+			<p class="aside-text">
+				
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic sapiente exercitationem quae. Quaerat labore animi quia cumque in atque, voluptatibus aliquam laboriosam fuga repellendus qui optio quae, soluta, beatae tenetur accusamus, adipisci explicabo veniam corporis consequuntur placeat itaque velit molestias dolores quos. Quaerat esse ad, accusamus? Nulla, unde!
+
+			</p>
+
+			<a href="#" class="btn-flip" data-back="Más información" data-front="Más información"></a>
+
+		</aside>
+
+		<section class="nosotros-image">
+
+			<div class="imgHolderv1 nosotros-img">
+				
+				<img src="<?php echo $ruta?>vistas/images/nosotros.png" alt="">
+
+			</div>
+
+		</section>
+
+	</main>
+
+</article>
