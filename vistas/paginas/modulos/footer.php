c9d3370f319c868e5b5550067c2566341dd6009d
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-10 16:18:15 -0600

    Creando la vista de la sección de footer

---
+++ vistas/paginas/modulos/footer.php
@@ -0,0 +1,126 @@
+<!--=============================================
+FOOTER
+=============================================-->
+
+<footer class="main-footer l-container">
+
+	<div class="footer-nav">
+		
+		<ul class="footer-menu">
+			
+			<li class="footer-menu__item">
+
+				<a href="#" class="footer-menu__link">¿Quiénes somos?</a>
+				
+			</li>
+
+			<li class="footer-menu__item">
+
+				<a href="#" class="footer-menu__link">Servicios</a>
+				
+			</li>
+
+			<li class="footer-menu__item">
+
+				<a href="#" class="footer-menu__link">VIP</a>
+				
+			</li>
+
+			<li class="footer-menu__item">
+
+				<a href="#" class="footer-menu__link">Contacto</a>
+				
+			</li>
+
+		</ul>
+
+	</div>
+
+	<div class="footer-redesSociales">
+		
+		<h4 class="footer-redesSociales__title">Conéctate con nosotros</h4> 
+
+		<div class="footer-redesSociales__links">			
+
+			<a href="https://www.facebook.com/profile.php?id=100080885433586" target="_blank">
+
+				<i class="fa-brands fa-square-facebook"></i>
+
+			</a>
+			
+			<a href="https://www.instagram.com/epiccarewash/" target="_blank">
+
+				<i class="fa-brands fa-instagram"></i>
+
+			</a>			
+
+		</div>
+
+	</div>
+
+</footer>
+
+<!--=============================================
+SECOND FOOTER
+=============================================-->
+
+<section class="second-footer">
+
+	<div class="second-footer__content">
+
+		<div class="second-footer__content--container l-container">
+			
+			<div class="second-footer__item">
+
+				<h3 class="item-title">Nuestra dirección</h3>
+
+				<p class="item-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque doloremque, culpa iure vel? Laborum, eius beatae reiciendis quos tempore iure.</p>
+				
+
+			</div>
+
+			<div class="second-footer__item">
+				
+				<h3 class="item-title"><h3 class="item-title">Horario de atención</h3>
+
+				<ul class="item-text">
+
+					<li>Sábados</li>
+
+					<li>6 PM - 9 PM</li>
+
+					<li>Domingos</li>
+
+					<li>7 AM - 1 PM</li>			
+
+				</ul>	
+
+			</div>
+
+			<div class="second-footer__item" style="margin-top : -30px;">
+				
+				<span class="item-phone">
+					
+					<i class="fa-brands fa-whatsapp"></i>
+
+					<h3 class="item-title"><h3 class="item-title" style="margin-top : -4px">+506 8540-0987</h3>
+
+				</span>	
+
+				<p class="item-text">					
+
+					Si tiene alguna pregunta, no dude en contactarnos.
+
+				</p>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<hr style="opacity: .3;">
+
+	<p class=" copyright center-content">Epic Carwash - Todos los derechos reservados © 2022 | Diseñado y desarrollado por Gustavo Solórzano Tinoco</p>
+
+</section>
