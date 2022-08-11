bb822abebdb5b0ac2839fe9195837b71eda42f1f
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-11 12:42:58 -0600

    Creando la vista de la sección animada de las imágenes before y after

---
+++ vistas/paginas/modulos/before_after.php
@@ -0,0 +1,41 @@
+<!--=============================================
+BEFORE AND AFTER IMAGE
+=============================================-->
+
+<article class="before_afterApp l-section">
+
+	<main class="before_afterMain l-container">
+		
+		<aside class="before_afterAside">
+			
+			<h4 class="before_afterSubtitle">Limpieza en seco</h4>
+
+			<h2 class="before_afterText">Limpiar en seco cualquier suciedad dentro del automóvil y el maletero.</h2>
+
+		</aside>
+
+		<section class="before_afterImg">
+
+					<figure class="cd-image-container">
+
+						<img src="<?php echo $ruta ?>vistas/images/after_01.jpg">
+
+						<span class="cd-image-label" data-type="original">DESPUÉS</span>
+
+						<div class="cd-resize-img"> <!-- the resizable image on top -->
+
+							<img src="<?php echo $ruta ?>vistas/images/before_01.jpg">
+
+							<span class="cd-image-label" data-type="modified">ANTES</span>
+
+						</div>
+
+						<span class="cd-handle"></span>
+
+				</figure>
+
+		</section>
+
+	</main>
+
+</article>
