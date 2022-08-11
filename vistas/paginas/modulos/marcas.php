356eee57106a3711f5a34fe7057de059e5d2b6ce
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-11 14:18:26 -0600

    Creando la vista de la sección de marcas

---
+++ vistas/paginas/modulos/marcas.php
@@ -0,0 +1,33 @@
+<div class="marcas l-container l-section">
+  
+  <div class="swiper mySwiper">
+
+  <div class="swiper-wrapper">
+
+    <div class="swiper-slide">
+      
+      <img src="<?php echo $ruta ?>vistas/images/marcas/marca1.png" alt="">
+
+    </div>
+
+    <div class="swiper-slide">
+      
+      <img src="<?php echo $ruta ?>vistas/images/marcas/marca2.png" alt="">
+
+    </div>
+
+    <div class="swiper-slide"><img src="<?php echo $ruta ?>vistas/images/marcas/marca3.png" alt=""></div>
+
+    <div class="swiper-slide"><img src="<?php echo $ruta ?>vistas/images/marcas/marca4.png" alt=""></div>
+
+    <div class="swiper-slide"><img src="<?php echo $ruta ?>vistas/images/marcas/marca6.png" alt=""></div>
+
+    <div class="swiper-slide"><img src="<?php echo $ruta ?>vistas/images/marcas/marca7.png"></div>
+
+
+  </div>
+
+</div>
+
+
+</div>
