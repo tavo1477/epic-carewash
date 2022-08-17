e557e4508a0725b2f6ac2940c344aca218830370
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-17 11:42:37 -0600

    Maquetando la página de nosotros

---
+++ vistas/paginas/nosotros.php
@@ -0,0 +1,72 @@
+<!--=====================================
+NOSOTROS
+======================================-->
+
+<section class="banner">
+    
+    <div class="banner-content">
+        
+        <h1 class="banner-content__title center-content">NOSOTROS</h1> 
+
+        <ul class="breadcrumb">
+
+          <li class="breadcrumb-item"><a href="<?php echo $ruta?>">Inicio</a></li>
+          <li class="breadcrumb-item">Nosotros</li>
+          
+        </ul>
+
+    </div>  
+
+</section>
+
+<section class="nosotros-container">
+
+  <h1 class="nosotros-container__title center-content">Sobre Epic Carewash</h1>
+
+  <p class="nosotros-container__text center-content">¿Quieres siempre un carro limpio? ¡Puedes lavar tu carro tantas veces como quieras con nosotros!</p>  
+
+</section>
+
+<article class="nosotrosApp l-section">
+    
+    <main class="nosotrosMain l-container">
+        
+        <aside class="nosotrosAside">
+
+            <div class="nosotrosAsideImg">
+                
+               <img src="<?php echo $ruta?>vistas/images/about.jpg" alt="Nosotros Image">  
+
+            </div>            
+
+        </aside>
+
+        <section class="nosotros-info">
+
+        <h4 class="nosotros-info__subTitle">Fundador</h4>
+
+        <h2 class="nosotros-info__title">Lavado y Limpieza Profesional <br> de tu auto</h2>
+
+        <p class="nosotros-info__text">Gabriel Canales técnico en soporte y mantenimiento de computadoras y estudiante de ingeniería crea Epic Carewash viendo la necesidad de brindar un servicio diferente con los mejores productos.</p>          
+
+        </section>
+
+    </main>
+
+</article>
+
+<!--=====================================
+NOSOTROS CERTIFICADO
+======================================-->
+
+<div class="nosotros-certificado">
+ 
+    <h2 class="nosotros-certificado__title center-content">Mi curriculum</h2>
+
+    <div class="nosotros-certificado__img center-content">
+            
+        <img src="<?php echo $ruta?>vistas/images/certificado.jpg" alt="Certificado">  
+
+    </div>
+
+</div>
