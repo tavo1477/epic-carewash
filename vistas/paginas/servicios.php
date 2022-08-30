0f539e8fd2cdf2173895c6d70efb0339f5aa0aa3
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-08-29 19:03:22 -0600

    Maquetando el módulo de servicios

---
+++ vistas/paginas/servicios.php
@@ -0,0 +1,230 @@
+<!--=====================================
+SERVICIOS
+======================================-->
+
+<section class="banner">
+    
+    <div class="banner-content">
+        
+        <h1 class="banner-content__title center-content">SERVICIOS</h1> 
+
+        <ul class="breadcrumb">
+
+          <li class="breadcrumb-item"><a href="<?php echo $ruta?>">Inicio</a></li>
+          <li class="breadcrumb-item">Servicios</li>
+          
+        </ul>
+
+    </div>  
+
+</section>
+
+<div class="serviciosBlock l-section">
+    
+    <h4 class="serviciosBlock-smallTitle center-content">CUIDAMOS TU COCHE</h4>
+    <h2 class="serviciosBlock-title center-content">Servicios que Brindamos</h2>
+    <p class="serviciosBlock-text center-content">Brindando servicios detallados y de lavado de autos sin comprometer la calidad.</p>
+
+    <section class="serviciosBlock-container l-section l-container">
+        
+        <article class="serviciosBlock-container__item">
+            
+            <div class="serviciosBlock-container__item--img">
+                
+                <img src="<?php echo $ruta?>vistas/images/servicios/services01.png" alt="">   
+
+            </div>
+
+            <div class="services-content">
+                
+                <h2 class="services-content__title">Lavado de autos</h2>
+                <p class="services-content__text">Buscamos la perfección de cada uno de nuestros lavados, utilizando las técnicas correctas para prevenir rayones en la carrocería e internamente, utilizamos guantes de lavados y utilizamos la técnica de "baldes" para dedicar en cada parte del vehículo.</p>
+
+            </div>
+
+        </article>       
+
+        <article class="serviciosBlock-container__item">
+            
+            <div class="serviciosBlock-container__item--img">
+                
+                <img src="<?php echo $ruta?>vistas/images/servicios/services02.png" alt="">   
+
+            </div>
+
+            <div class="services-content">
+                
+                <h2 class="services-content__title">Descontaminación química y mecánica de pintura </h2>
+                <p class="services-content__text">Mecánica: en este proceso se utiliza  una "clay bar" junto con un lubricante para poder deslizar la clay bar y así eliminar los  contaminantes de la pintura . 
+                Química: en este proceso se utilizan productos que remueven de una manera más rápida los contaminantes</p>
+
+            </div>
+
+        </article>
+
+        <article class="serviciosBlock-container__item">
+            
+            <div class="serviciosBlock-container__item--img">
+                
+                <img src="<?php echo $ruta?>vistas/images/servicios/services03.png" alt="">   
+
+            </div>
+
+            <div class="services-content">
+                
+                <h2 class="services-content__title">Pulido de vidrios y de focos </h2>
+                <p class="services-content__text"> Pulido de vidrios se realiza para eliminar cualquier imperfección  cómo rayones, gota seca entre otros. Y pulido de focos consiste en restaurar la condición de los focos dejándolos en su estado original, aplicando un sellador.</p>
+
+            </div>
+
+        </article>       
+
+    </section>
+
+</div>
+
+<div class="servicesBanner l-section">   
+
+    <div class="servicesBanner__content l-container">
+
+        <div class="content-information">
+
+            <div class="content-iconInformation">                
+
+               <img src="<?php echo $ruta?>vistas/images/iconBanner.png" alt=""> 
+
+            </div>          
+          
+            <div class="content-desc">
+                
+                <h2 class="content-desc__title">¿Necesitas ayuda con tu vehículo?</h2>
+
+               <p class="content-desc__text">Envíenos un mensaje o llámenos al +506 8540-0987</p>  
+
+            </div>
+
+        </div>
+        
+        <div class="content-button">
+          
+           <a href="contacto" class="btn-flip" data-back="Ahora mismo" data-front="Contáctenos"></a>  
+
+        </div>       
+
+    </div>      
+
+</div>
+
+<section class="servicesOptions">
+
+    <article class="servicesOptions-container l-container">
+
+        <aside class="servicesOptions-container__desc">
+            
+            <h2 class="servicesOptions-title">Epic Carewash le ofrece una variedad de opciones</h2>
+
+            <p class="servicesOptions-text">Nuestro servicio le ofrece una variedad de opciones para satisfacer sus necesidades y la estimación de servicio más precisa y de precio justo.</p>
+
+        </aside>
+
+        <div class="servicesOptions-container__video">
+
+             <div class="video-container">            
+
+                <a data-fancybox href="https://youtube.com/watch?v=Mki1Wd6IAm4?feature=share">
+
+                   <i class="fa-solid fa-play"></i>
+
+                </a>
+
+             </div>
+
+        </div>
+
+        <ul class="servicesOptions-container__list">
+
+            <li class="servicesOptions-item">
+                
+                <i class="fa-solid fa-check"></i>
+
+                <h3 class="item-title">Limpieza interna </h3>
+
+            </li>
+
+            <li class="servicesOptions-item">
+                
+                <i class="fa-solid fa-check"></i>
+
+                <h3 class="item-title">Detallado de partes negras </h3>
+
+            </li>
+
+            <li class="servicesOptions-item">
+                
+                <i class="fa-solid fa-check"></i>
+
+                <h3 class="item-title">Abrillantado de llantas</h3>
+
+            </li>
+
+             <li class="servicesOptions-item">
+                
+                <i class="fa-solid fa-check"></i>
+
+                <h3 class="item-title">Eliminación de gota seca</h3>
+
+            </li>
+
+        </ul>
+
+    </article>    
+
+</section>
+
+<section class="servicesPaying"> 
+   
+       <img src="<?php echo $ruta?>vistas/images/bgPattern03.png" class="servicesPaying__hero-img"  alt=""> 
+
+       <div class="paying-content l-container">
+
+         <div class="servicesPaying-item">
+
+            <div class="payingOption">
+                
+                <h2 class="paying-number">1</h2>
+
+            </div>            
+            
+
+            <p class="paying-text">Pide cita y trae tu auto</p>
+
+        </div>
+
+        <div class="servicesPaying-item">
+            
+            <div class="payingOption">
+                
+                <h2 class="paying-number">2</h2>
+
+            </div>  
+
+            <p class="paying-text">Damos nuestra calificación de 5 estrellas</p>
+
+        </div>
+
+        <div class="servicesPaying-item">
+            
+             <div class="payingOption">
+                
+                <h2 class="paying-number">3</h2>
+
+            </div>  
+
+            <p class="paying-text">Puede pagar por medio de SINPE o por Paypal</p>
+
+        </div>
+
+
+       </div>  
+
+</section>
