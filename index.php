d832d294e2405dc5b0c66fdd3fedf83ad2416c2a
Gustavo Solórzano <gustinoc1977@gmail.com>
2022-09-02 13:23:05 -0600

    Vinculando el controlador de correo

--- index.php
+++ index.php
@@ -3,5 +3,7 @@
 require_once "controladores/plantillaControlador.php";
 require_once "controladores/rutaControlador.php";
 
+require_once "controladores/correoControlador.php";
+
 $plantilla = new ControladorPlantilla();
 $plantilla -> ctrPlantilla();
